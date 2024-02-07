876a2c9551714ef9b218ffe60df57f6a47b05dd2
Dakota Newman <dakota.newman@yahoo.com>
2024-02-06 22:05:38 -0500

    added bootstrap to project, started on menu page to start testing how items look

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,49 +8,69 @@ import Menu from './Pages/Menu'
 import Gallery from './Pages/Gallery'
 import LocalFarmers from './Pages/LocalFarmers'
 import Contact from './Pages/Contact'
-
-const router = createBrowserRouter([
-  {
-    path : '/',
-    element : <Layout />,
-    children : [
-      {
-        index : true,
-        element : <Welcome />
-      },
-      {
-        path : '/login',
-        element : <Login />
-      },
-      {
-        path : '/about',
-        element : <About />
-      },
-      {
-        path : '/menu',
-        element : <Menu />
-      },
-      {
-        path : '/gallery',
-        element : <Gallery /> 
-      },
-      {
-        path : '/localfarmers',
-        element : <LocalFarmers />
-      },
-      {
-        path : '/contact',
-        element : <Contact />
-      }
-    ]
-  }
-])
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const menuItemsEndpoint = import.meta.env.VITE_MENUITEMS_ENDPOINT
+  
+  const [menuItems, setMenuItems] = useState([])
+
+
+  useEffect(() => {
+    axios.get(`${menuItemsEndpoint}/all-menu-items`)
+          .then((res) => {
+            setMenuItems(res.data.menuItems)
+          })
+          .catch((err) => console.log(err))
+  }, [])
+
+  const router = createBrowserRouter([
+    {
+      path : '/',
+      element : <Layout />,
+      children : [
+        {
+          index : true,
+          element : <Welcome />
+        },
+        {
+          path : '/login',
+          element : <Login />
+        },
+        {
+          path : '/about',
+          element : <About />
+        },
+        {
+          path : '/menu',
+          element : <Menu 
+                      menuItems={menuItems}
+                    />
+        },
+        {
+          path : '/gallery',
+          element : <Gallery /> 
+        },
+        {
+          path : '/localfarmers',
+          element : <LocalFarmers />
+        },
+        {
+          path : '/contact',
+          element : <Contact />
+        }
+      ]
+    }
+  ])
 
   return (
       <RouterProvider router={router} />
   )
 }
+
+
+
 
 export default App
--- client/src/Pages/Menu.jsx
+++ client/src/Pages/Menu.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-const Menu = () => {
+const Menu = (props) => {
+
+  console.log(props)
+
   return (
 
     <>
-        <h1>Menu</h1>
+        <Container fluid='md'>
+          <Row className='justify-content-center text-center'>
+            <Col>
+              <h1>Appetizers</h1>
+            </Col>
+          </Row>
+        </Container>
     </>
   )
 }
