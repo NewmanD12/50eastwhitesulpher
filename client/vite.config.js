27b5aa0d939d21a03b3e40b92367e2173eed8a14
Dakota Newman <dakota.newman@yahoo.com>
2024-03-16 11:39:39 -0400

    trying new config settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server : {
-    host : true,
-    port : 80,
-  }
+config: JSON.stringify({
+  apiUrl: '/api'
 })
